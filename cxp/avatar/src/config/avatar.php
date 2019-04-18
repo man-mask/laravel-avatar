22d5100a9b492cfebb51e69d7a27084f0e2d1994
vagrant <vagrant@homestead>
2019-04-18 03:16:28 +0000

    change the project

--- cxp/avatar/src/config/avatar.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return   [
-    'type'      => 'png',                                         // jpeg|png|gif|bmp
-    'width'     => '100',
-    'height'    => '100',
-    'size'      => '26',
-    'font_file' => public_path() . '/fonts/WawaSC-Regular.otf',
-];
